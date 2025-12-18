1732bfd7fce387aabe5e9bb0b3768b2827806cc0
Kangyelim1 <gyelim887@gmail.com>
2025-12-19 05:26:07 +0900

    빌드

--- Assets/Crafting/CraftingPanel.cs
+++ Assets/Crafting/CraftingPanel.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Text;
-using UnityEditor.UI;
-using TMPro;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class CraftingPanel : MonoBehaviour
 {
